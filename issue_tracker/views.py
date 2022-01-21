3b03cb39424b61f1711e950dba83d22747ba2baf
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-01-21 15:35:19 +0600

    #3 Создал представление для создания новой задачи

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
+
+from issue_tracker.forms import IssueModelForm
 from issue_tracker.models import Issue
 
 
@@ -11,3 +13,11 @@ class IndexView(TemplateView):
         context['issues'] = Issue.objects.all()
         return context
 
+
+class CreateIssueView(TemplateView):
+    template_name = 'create_issue.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = IssueModelForm()
+        return context
