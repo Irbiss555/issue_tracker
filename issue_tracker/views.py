029f5cb36a8061298d7bacc22b716390429dd750
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-01-21 18:12:59 +0600

    #5 Добавил обработку post запроса в представлении

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
-
 from issue_tracker.forms import IssueModelForm
 from issue_tracker.models import Issue
 
@@ -29,13 +28,13 @@ class CreateIssueView(TemplateView):
             description = form.cleaned_data['description']
             status = form.cleaned_data['status']
             type = form.cleaned_data['type']
-            Issue.objects.create(
+            issue = Issue.objects.create(
                 summary=summary,
                 description=description,
                 status=status,
                 type=type
             )
-            return redirect('index')
+            return redirect('detail_issue', pk=issue.pk)
         else:
             context = super().get_context_data(**kwargs)
             context['form'] = form
@@ -59,3 +58,19 @@ class EditIssueView(TemplateView):
         context['form'] = IssueModelForm()
         context['issue'] = get_object_or_404(Issue, pk=kwargs['pk'])
         return context
+
+    def post(self, request, *args, **kwargs):
+        issue = get_object_or_404(Issue, pk=kwargs['pk'])
+        form = IssueModelForm(request.POST)
+        if form.is_valid():
+            issue.summary = form.cleaned_data['summary']
+            issue.description = form.cleaned_data['description']
+            issue.status = form.cleaned_data['status']
+            issue.type = form.cleaned_data['type']
+            issue.save(update_fields=['summary', 'description', 'status', 'type'])
+            return redirect('detail_issue', pk=issue.pk)
+        else:
+            context = super().get_context_data(**kwargs)
+            context['form'] = form
+            context['issue'] = issue
+            return super().render_to_response(context=context)
