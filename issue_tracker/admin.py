7aad842fb9c907edf33d0e8d327824daf2abb49b
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-01-21 14:15:14 +0600

    #1 Зарегистрировал модели в админ-панели

--- issue_tracker/admin.py
+++ issue_tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from issue_tracker.models import Status, Type, Issue
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'status', 'type', 'updated_at']
+    search_fields = ['summary']
+    fields = ['summary', 'status', 'type', 'description', 'updated_at', 'created_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Issue, IssueAdmin)
