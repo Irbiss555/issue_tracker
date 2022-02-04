75635e59a3d3f39e81f8317c710708ec1747ac0d
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-04 14:24:23 +0600

    #11 Создал форму для поиска

--- issue_tracker/forms.py
+++ issue_tracker/forms.py
@@ -1,9 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 
 from issue_tracker.models import Issue
 
 
-class IssueModelForm(ModelForm):
+class IssueModelForm(forms.ModelForm):
     class Meta:
         model = Issue
         fields = ['summary', 'description', 'status', 'type']
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Search")
