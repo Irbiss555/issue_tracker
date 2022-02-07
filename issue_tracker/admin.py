36163df3f97854fccde39e0e20c89d18a85c9c97
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-08 02:17:31 +0600

    #13 Добавил поле проекта задачи в админ-панели

--- issue_tracker/admin.py
+++ issue_tracker/admin.py
@@ -7,7 +7,7 @@ from issue_tracker.models import Status, Type, Issue
 class IssueAdmin(admin.ModelAdmin):
     list_display = ['summary', 'status', 'type', 'updated_at']
     search_fields = ['summary']
-    fields = ['summary', 'status', 'type', 'description', 'updated_at', 'created_at']
+    fields = ['summary', 'project', 'status', 'type', 'description', 'updated_at', 'created_at']
     readonly_fields = ['created_at', 'updated_at']
 
 
