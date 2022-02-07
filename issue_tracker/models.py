a3548c6b9e54e1c22771a20f72f58840bba22b17
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-08 01:33:05 +0600

    #13 Создал модель проекта Project

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -4,6 +4,22 @@ from django.db import models
 from issue_tracker.validators import MinLengthValidator, MaxLengthValidator
 
 
+class Project(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Title', validators=(MinLengthValidator(3),))
+    description = models.TextField(
+        max_length=800,
+        null=True,
+        blank=True,
+        verbose_name='Description',
+        validators=(MinLengthValidator(8),)
+    )
+    start_date = models.DateField(verbose_name='Start date')
+    end_date = models.DateField(null=True, blank=True, verbose_name='End date')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+
 class Issue(models.Model):
     summary = models.CharField(max_length=500, verbose_name='Summary', validators=(MaxLengthValidator(20),))
     description = models.TextField(
