b0e3096dd557bd5be77a4699e172ac31c654503f
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-08 15:36:18 +0600

    #16 Создал представление для создания нового проекта

--- issue_tracker/views/project_views.py
+++ issue_tracker/views/project_views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from issue_tracker.forms import ProjectModelForm
 from issue_tracker.models import Project
 
 
@@ -13,3 +15,12 @@ class ProjectView(DetailView):
     template_name = 'project/project_detail.html'
     model = Project
     context_object_name = 'project'
+
+
+class ProjectCreateView(CreateView):
+    template_name = 'project/project_create.html'
+    model = Project
+    form_class = ProjectModelForm
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.object.pk})
