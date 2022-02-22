d1d4cea92f9d2f8bdc90977e11beb5701a5bbb56
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-22 14:08:27 +0600

    #25 Включил роуты из приложений в основной urls

--- core/urls.py
+++ core/urls.py
@@ -14,45 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from issue_tracker.views import (
-    IssueListView, DetailIssueView,
-    IssueEditView, DeleteIssueView, IssueCreateView
-)
-from issue_tracker.views.project_views import (
-    ProjectListView, ProjectView, ProjectCreateView,
-    ProjectIssueCreateView, ProjectEditView, ProjectDeleteView)
-
-from django.contrib.auth.views import LoginView, LogoutView
-
-
-accounts_urls = [
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
-]
-
-issue_urls = [
-    path('issues/', IssueListView.as_view(), name='issue_list'),
-    path('issue/create/', IssueCreateView.as_view(), name='create_issue'),
-    path('issue/<int:pk>', DetailIssueView.as_view(), name='detail_issue'),
-    path('issue/edit/<int:pk>', IssueEditView.as_view(), name='edit_issue'),
-    path('issue/delete/<int:pk>', DeleteIssueView.as_view(), name='delete_issue'),
-]
-
-project_urls = [
-    path('', ProjectListView.as_view(), name='project_list'),
-    path('project/<int:pk>', ProjectView.as_view(), name='project_detail'),
-    path('project/create/', ProjectCreateView.as_view(), name='project_create'),
-    path('project/<int:pk>/edit/', ProjectEditView.as_view(), name='project_edit'),
-    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
-    path('project/<int:pk>/issue/create/', ProjectIssueCreateView.as_view(), name='project_issue_create'),
-]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', include('issue_tracker.urls'))
 ]
-
-urlpatterns.extend(accounts_urls)
-urlpatterns.extend(issue_urls)
-urlpatterns.extend(project_urls)
