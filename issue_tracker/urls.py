40104c6d996b2fa64193eb0113c3c38221044ead
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-22 14:06:09 +0600

    #25 Вынес роуты для приложения issue_tracker в отдельный файл

---
+++ issue_tracker/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+
+from issue_tracker.views import (
+    IssueListView, DetailIssueView,
+    IssueEditView, DeleteIssueView, IssueCreateView
+)
+from issue_tracker.views.project_views import (
+    ProjectListView, ProjectView, ProjectCreateView,
+    ProjectIssueCreateView, ProjectEditView, ProjectDeleteView)
+
+
+issue_urls = [
+    path('issues/', IssueListView.as_view(), name='issue_list'),
+    path('issue/create/', IssueCreateView.as_view(), name='create_issue'),
+    path('issue/<int:pk>', DetailIssueView.as_view(), name='detail_issue'),
+    path('issue/edit/<int:pk>', IssueEditView.as_view(), name='edit_issue'),
+    path('issue/delete/<int:pk>', DeleteIssueView.as_view(), name='delete_issue'),
+]
+
+project_urls = [
+    path('', ProjectListView.as_view(), name='project_list'),
+    path('project/<int:pk>', ProjectView.as_view(), name='project_detail'),
+    path('project/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('project/<int:pk>/edit/', ProjectEditView.as_view(), name='project_edit'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('project/<int:pk>/issue/create/', ProjectIssueCreateView.as_view(), name='project_issue_create'),
+]
+
+urlpatterns = issue_urls + project_urls
