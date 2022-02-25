ebc9d8960cb9bf28a57a0ebb622c478c92ecfc57
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-25 19:03:39 +0600

    #23 Добавил кастомное представление для входа пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
+from django.shortcuts import redirect, resolve_url
 from django.urls import reverse
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 
 from accounts.forms import MyUserCreationForm
+from django.conf import settings
 
 
 class RegisterView(CreateView):
@@ -29,3 +31,21 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('issue_tracker:project_list')
         return next_url
+
+
+class LoginUserView(LoginView):
+    redirect_authenticated_user = True
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['login_page'] = 'True'
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        print(next_url)
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        print(next_url)
+        print(resolve_url(settings.LOGIN_REDIRECT_URL))
+        return next_url or resolve_url(settings.LOGIN_REDIRECT_URL)
