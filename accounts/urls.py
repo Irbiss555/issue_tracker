35f11e30794eca9b7881379e3c5523171425a941
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-02-25 19:04:09 +0600

    #23 Изменил роут для кастомного представления входа

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, LoginUserView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
 ]
