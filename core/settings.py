513e4541439538251eaabca0ab41e7548b815b18
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 00:04:52 +0600

    #28 Добавил url и путь для медиа файлов в settings.py

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+MEDIA_URL = 'uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'issue_tracker/static'
