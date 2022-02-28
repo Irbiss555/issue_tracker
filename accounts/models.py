7c26874d168bff3b817679d4f9170798d273f1b3
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 00:02:30 +0600

    #28 Создал модель профиля для пользователя

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=get_user_model(),
+        related_name='profile',
+        on_delete=models.CASCADE,
+        verbose_name='Profile',
+    )
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='user_pics',
+        verbose_name='Avatar',
+    )
+    github_link = models.URLField(
+        null=True,
+        blank=True,
+        verbose_name='GitHub',
+    )
+    about_info = models.TextField(
+        max_length=2000,
+        null=True,
+        blank=True,
+        verbose_name='About info',
+    )
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        
