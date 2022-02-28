14ee3fce837bec670737a4ab5b64bbf0b8986dda
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 00:09:57 +0600

    #28 Добавил роуты для медиа файлов в urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls.static import static
 from django.urls import path
 
 from django.contrib.auth.views import LogoutView
 
 from accounts.views import RegisterView, LoginUserView
+from core import settings
 
 app_name = 'accounts'
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
