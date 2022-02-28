8cbfe91316399355e7c073ca528ba17c33ad36fc
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 00:22:11 +0600

    #28 Добавил профиль пользователя в админ панель

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import Profile
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'github_link', 'about_info']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInLine]
+
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
