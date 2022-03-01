d022a892bac9600deda00ef11d196e468ca0d6ca
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 11:15:07 +0600

    #29 Создал представление для просмотра профиля пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, resolve_url
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.views import LoginView
 
 from accounts.forms import MyUserCreationForm
@@ -46,3 +48,27 @@ class LoginUserView(LoginView):
         if not next_url:
             next_url = self.request.POST.get('next')
         return next_url or resolve_url(settings.LOGIN_REDIRECT_URL)
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_profile.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        projects = self.object.projects.order_by('start_date')
+        paginator = Paginator(
+            projects,
+            self.paginate_related_by,
+            orphans=self.paginate_related_orphans
+        )
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
+
+
