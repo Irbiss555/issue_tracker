8f2ba61beda7888beaaa15a06bac11515b4d713a
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-01 13:07:59 +0600

    #29 Перекинул роут для медиа файлов из приложения accounts в core

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls.static import static
 from django.urls import path
 
 from django.contrib.auth.views import LogoutView
 
-from accounts.views import RegisterView, LoginUserView
-from core import settings
+from accounts.views import RegisterView, LoginUserView, UserProfileView
+
 
 app_name = 'accounts'
 
@@ -12,4 +11,5 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('profile/<int:pk>', UserProfileView.as_view(), name='user_profile'),
+]
