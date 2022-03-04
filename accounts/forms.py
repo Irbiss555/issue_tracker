8c23d428c761e35b20a416e0757cb56f27fbe6a7
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-04 13:33:37 +0600

    #31 Создал форму для редактирования профиля

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django import forms
 
+from accounts.models import Profile
+
 
 class MyUserCreationForm(forms.ModelForm):
     password = forms.CharField(
@@ -52,3 +54,9 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileChangeForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
