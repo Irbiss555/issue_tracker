5f050e447cd980406e76ba8926e4c6279cca0e9c
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-04 13:31:29 +0600

    #31 Создал форму для редактирования пользователя

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django import forms
 
@@ -45,3 +46,9 @@ class MyUserCreationForm(forms.ModelForm):
             'last_name',
             'email',
         ]
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
