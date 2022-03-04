3680829e6dc89c4dd1c71c0769a340a0b9b84bbb
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-04 13:56:23 +0600

    #31 Создал представление для редактирования пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, resolve_url
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.contrib.auth.views import LoginView
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm
 from django.conf import settings
 
 
@@ -77,3 +77,43 @@ class UserListView(PermissionRequiredMixin, ListView):
     model = get_user_model()
     context_object_name = 'user_list'
     permission_required = 'auth.view_user'
+
+
+class UserUpdateView(UpdateView):
+    template_name = 'user_edit.html'
+    model = get_user_model()
+    context_object_name = 'user_obj'
+    form_class = UserChangeForm
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super(UserUpdateView, self).get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        response = super().form_valid(form)
+        profile_form.save()
+        return response
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileChangeForm(**form_kwargs)
+
+    def get_success_url(self):
+        return reverse('accounts:user_profile', kwargs={'pk': self.object.pk})
