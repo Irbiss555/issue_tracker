a10881437d707747d0f29bb6331d9361adb43704
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-04 22:53:34 +0600

    #33 Создал сериализаторы для проекта и задач

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from issue_tracker.models import Project, Issue
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            'id',
+            'users',
+            'title',
+            'descriptions',
+            'start_date',
+            'end_date'
+        ]
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = [
+            'id',
+            'project',
+            'summary',
+            'description',
+            'status',
+            'type',
+            'created_at',
+            'updated_at',
+        ]
