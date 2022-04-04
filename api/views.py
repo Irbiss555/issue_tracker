e6eea463fea76c47ea040220294fc6b78407a8dc
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-04 23:57:20 +0600

    #33 Реализовал API представление для обновления проекта

--- api/views.py
+++ api/views.py
@@ -8,30 +8,18 @@ from issue_tracker.models import Project, Issue
 
 
 class ProjectApiView(APIView):
-    # template_name = 'project/project_detail.html'
-    # model = Project
-    # context_object_name = 'project'
     def get(self, request, *args, **kwargs):
         object = get_object_or_404(Project, pk=self.kwargs.get('pk'))
         serializer = ProjectSerializer(object)
         return Response(serializer.data)
 
-
-
-# class ProjectEditView(APIView):
-#     template_name = 'project/project_edit.html'
-#     model = Project
-#     form_class = ProjectModelForm
-#     context_object_name = 'project'
-#     permission_required = 'issue_tracker.change_project'
-#
-#     def get_success_url(self):
-#         return reverse('issue_tracker:project_detail', kwargs={'pk': self.object.pk})
-#
-#
-# class ProjectDeleteView(APIView):
-#     template_name = 'project/project_detail.html'
-#     model = Project
-#     success_url = reverse_lazy('issue_tracker:project_list')
-#     context_object_name = ''
-#     permission_required = 'issue_tracker.delete_project'
+    def put(self, request, *args, **kwargs):
+        serializer = ProjectSerializer(
+            data=request.data,
+            instance=get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        )
+        if serializer.is_valid():
+            project = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
