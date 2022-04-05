1e57a4d4de3ff3b37fe2e119c2c700e459c19641
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-05 14:28:49 +0600

    #33 Добавил эндпоинт для API задач

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import ProjectApiView
+from api.views import ProjectApiView, IssueApiView
 
 urlpatterns = [
     path('projects/<int:pk>', ProjectApiView.as_view()),
+    path('issues/<int:pk>', IssueApiView.as_view()),
 ]
